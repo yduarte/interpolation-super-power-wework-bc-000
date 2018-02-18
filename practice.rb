102f4e297dee340aecd8026e21786e3da3154eab
Yanllerline Duarte <yanllerline.duarte@wework.com>
2018-02-18 21:48:22 +0000

    Done.

---
+++ practice.rb
@@ -0,0 +1,11 @@
+venesa=["Venesa",29,"teacher"]
+michelle=["Michelle",27,"dentist"]
+yange=["Yange",30,"project expediter"]
+
+
+def print_business_card(individual)
+  puts "Name:#{individual[0]},Age:#{individual[1]},Occupation #{individual[2]}"
+  end
+  
+  print_business_card(michelle)
+ 
